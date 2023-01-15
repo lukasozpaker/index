5cd41f8859a0631f22219e75db69c6f46d09ec40
lukasozpaker <lukas.ozpaker@gmail.com>
2023-01-15 14:28:52 -0500

    Testing Stripe API

--- src/components/undertitle.tsx
+++ src/components/undertitle.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./style.css";
 
 
+function test(){
+    alert("This is a test");
+}
+
 export class UnderTitle extends React.Component {
     constructor(props:any){
         super(props);
@@ -15,16 +19,20 @@ export class UnderTitle extends React.Component {
         return (
             <div className="underTitle">
 
-
-                <a href="https://www.github.com/lukasozpaker">
-                    Github
-                    <div></div>
-                </a>
-                <a href="https://www.linkedin.com/in/lukasozpaker">
-                    LinkedIn
-                    <div></div>
-                </a>
-
+                <div className="link-cont">
+                    <a href="https://www.github.com/lukasozpaker">
+                        Github
+                        <div></div>
+                    </a>
+                    <a href="https://www.linkedin.com/in/lukasozpaker">
+                        LinkedIn
+                        <div></div>
+                    </a>
+                    <a href="https://buy.stripe.com/test_9AQdRd6hEePA62ccMM" /*onClick={test}*/>
+                        Stripe
+                        <div></div>
+                    </a>
+                </div>
 
                 <p>
                     Below are a number of web and mobile applications I've worked on in the past. Some are still in development and a few are now deprecated due to API changes among other reasons. The experimental command line is currently not intended for use so use it at your own risk.
